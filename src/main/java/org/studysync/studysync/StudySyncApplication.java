a5621f596358f4eb3e20a87a40ae0fe1fda283b0
minnnisu <mine52100@naver.com>
2024-11-01 15:56:21 +0900

    init: 프로젝트 초기 설정

---
+++ src/main/java/org/studysync/studysync/StudySyncApplication.java
@@ -0,0 +1,13 @@
+package org.studysync.studysync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudySyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/studysync/studysync/StudySyncApplicationTests.java
@@ -0,0 +1,13 @@
+package org.studysync.studysync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
